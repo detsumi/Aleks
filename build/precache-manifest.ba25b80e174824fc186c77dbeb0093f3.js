7a1ed229109018be440c76f18df6ca56b53a8d9b
detsumi <50370998+detsumi@users.noreply.github.com>
2019-05-07 17:26:37 +0300

    Add files via upload

---
+++ build/precache-manifest.ba25b80e174824fc186c77dbeb0093f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38540dfc44e1deb6b2ca",
+    "url": "/static/js/main.29174df5.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "38540dfc44e1deb6b2ca",
+    "url": "/static/css/main.50f9b8f5.chunk.css"
+  },
+  {
+    "revision": "2ca22dcb73cfecc40108137f75bba0e9",
+    "url": "/index.html"
+  }
+];
